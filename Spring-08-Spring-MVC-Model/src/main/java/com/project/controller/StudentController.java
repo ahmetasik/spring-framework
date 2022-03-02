4953f0c5de240dba909025b6df04e6bea75c9e8f
ahmetasik <ahmetfza@gmail.com>
2022-03-01 21:45:01 -0500

    Thymleaf

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/project/controller/StudentController.java
@@ -0,0 +1,18 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping("/welcome")
+    public String homePage(Model model){
+
+        model.addAttribute("name","Cydeo");
+        model.addAttribute("course","MVC");
+
+        return "student/welcome";
+    }
+}
